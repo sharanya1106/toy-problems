efc4871c2d91712e9ce2805752a2cb37b6f2eeaa
sharanya <sharanyarao49@msitprogram.net>
2020-04-15 11:10:56 +0530

    added 3 methods

--- lrucache.py
+++ lrucache.py
@@ -3,3 +3,12 @@ class lrucache:
     def __init__(self, capacity):
         self.capacity = capacity
         self.cache_dict = OrderedDict()
+
+    def get(self,key):
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def get_cache(self):
+        pass
